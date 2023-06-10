0c466d10b6cfbe6990a53f8fb7bf602247b66d8c
강명진 <rkdwlss2@gmail.com>
2023-06-10 19:33:43 +0900

    changed

--- src/test/java/com/cos/book/web/BookControllerIntegreTest.java
+++ src/test/java/com/cos/book/web/BookControllerIntegreTest.java
@@ -1,5 +1,6 @@
 package com.cos.book.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 //통합테스트 (모든 Bean을 똑같이 Ioc에 올리고 테스트하기) 느림
 // springBootTest안에도 @ExtendWith({SpringExtension.class})있다.
 // WebEnvironment.RANDOM_PORT에서는 실제 톰켓으로 테스트
+@Slf4j
 @Transactional // 각각 테스트함수가 종료해줄때마다 트랜잭션을 rollback 해주는 어노테이션
 @AutoConfigureMockMvc // 이게 있어야 MockMvc를 IOC에 등록해줌(넣어줌)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)// MOCK은 실제 톰켓이 아니라 다른톰켓으로 테스트
@@ -19,5 +21,8 @@ public class BookControllerIntegreTest {
     @Autowired
     private MockMvc mockMvc;
 
-
+    @Test
+    public void save_테스트(){
+        log.info("save_테스트() 시작====================================");
+    }
 }
--- src/test/java/com/cos/book/web/BookControllerUnitTest.java
+++ src/test/java/com/cos/book/web/BookControllerUnitTest.java
@@ -1,11 +1,39 @@
 package com.cos.book.web;
 
+import com.cos.book.domain.Book;
+import com.cos.book.service.BookService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 // 실제 서버에서테스트 해볼때 완벽하게 돌아가는지 확인 불가 전체가 안뜨니깐 가볍다.
 // @ExtendWith({SpringExtension.class}) 스프링으로 확장하려면 무조건 써야한다. junit4에서는 ExtendWith이 WebMvcTest에 업섰는뎅 5부터는 들어왔다.
 // Controller, filter,Advice만 올라간다.
+@Slf4j
 @WebMvcTest
 public class BookControllerUnitTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
 
+    @MockBean // ioc환경에 bean등록됨 실제로 동작 안하는 서비스
+    private BookService bookService;
+
+    // BDDMockito 패턴
+    @Test
+    public void save_테스트() throws JsonProcessingException {
+        // given (테스트를 하기 위한 준비)
+        String content = new ObjectMapper().writeValueAsString(new Book(null,"스프링 따라하기","코스"));
+        log.info(content);
+
+//        log.info("save_테스트() 시작====================================");
+//        Book book = bookService.저장하기(new Book(null,"제목","코스"));
+//        System.out.println("book : "+ book);
+    }
 }
